22befe672126c6b1c2c896f3d802d59cf7eccaf6
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-25 18:45:09 +0600

    server-initilized

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-
+require('dotenv').config()
 //middleware
 
 app.use(cors())
 app.use(express.json())
+
+
+
+//mongodb
+
+
 
 
 
